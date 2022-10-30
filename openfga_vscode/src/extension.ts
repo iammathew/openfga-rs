c4264930be66da184167da1d7813e8e584557f43
iammathew <23657891+iammathew@users.noreply.github.com>
2022-10-30 15:25:03 +0100

    chore: add basic setup for vscode extension

---
+++ openfga_vscode/src/extension.ts
@@ -0,0 +1,64 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const command = process.env.SERVER_PATH || "openfgalsp";
+  const run: Executable = {
+    command,
+    options: {
+      env: {
+        ...process.env,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        RUST_LOG: "debug",
+      },
+    },
+  };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "openfga" }],
+    synchronize: {
+      // Notify the server about file changes to '.openfgarc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.openfgarc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "OpenFGALSP",
+    "OpenFGA Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
