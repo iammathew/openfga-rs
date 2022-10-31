8bfdd0afaa1d873b73a44d85b1282f6968682fef
iammathew <23657891+iammathew@users.noreply.github.com>
2022-10-31 17:59:43 +0100

    feat: add more boilerplate to lsp

--- openfga_lsp/src/main.rs
+++ openfga_lsp/src/main.rs
@@ -1,3 +1,6 @@
+use dashmap::DashMap;
+use openfga_common::AuthorizationModel;
+use openfga_dsl_parser::Token;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
@@ -5,6 +8,8 @@ use tower_lsp::{Client, LanguageServer, LspService, Server};
 #[derive(Debug)]
 struct Backend {
     client: Client,
+    model_map: DashMap<String, Option<AuthorizationModel>>,
+    token_map: DashMap<String, Option<Vec<Token>>>,
 }
 
 #[tower_lsp::async_trait]
@@ -12,6 +17,9 @@ impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
                 completion_provider: Some(CompletionOptions::default()),
                 ..Default::default()
@@ -32,8 +40,8 @@ impl LanguageServer for Backend {
 
     async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
         Ok(Some(CompletionResponse::Array(vec![
-            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
-            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
+            CompletionItem::new_simple("type".to_string(), "New type".to_string()),
+            CompletionItem::new_simple("define".to_string(), "New relation".to_string()),
         ])))
     }
 
@@ -43,6 +51,39 @@ impl LanguageServer for Backend {
             range: None,
         }))
     }
+
+    async fn did_open(&self, _: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+    }
+
+    async fn did_change(&self, _: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file changed!")
+            .await;
+    }
+
+    async fn did_save(&self, _: DidSaveTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file saved!")
+            .await;
+    }
+
+    async fn did_close(&self, _: DidCloseTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file closed!")
+            .await;
+    }
+}
+
+impl Backend {
+    fn on_change(&self) {
+        self.model_map.insert(
+            "Test".into(),
+            Some(AuthorizationModel { types: Vec::new() }),
+        );
+    }
 }
 
 #[tokio::main]
@@ -50,6 +91,10 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        model_map: DashMap::new(),
+        token_map: DashMap::new(),
+    });
     Server::new(stdin, stdout, socket).serve(service).await;
 }
